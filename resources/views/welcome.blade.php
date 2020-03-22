b211a87da8a8c7100eb5d7031c61a7edabdf79c6
geremyns <geremysaico@gmail.com>
2020-03-22 15:02:42 -0300

    :penguin: fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Porfin funciono esta wea
+                    Porfin funciono esta weas
                 </div>
                 <div>
                     <small>Hace puro sueño hasta mañana</small>
