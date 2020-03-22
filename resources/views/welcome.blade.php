d48032671f0f48cec0e8460f5677365ed9ca75b8
geremyns <geremysaico@gmail.com>
2020-03-22 03:49:11 -0300

    :penguin: Fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,9 @@
                 <div class="title m-b-md">
                     Porfin funciono esta wea
                 </div>
+                <div>
+                    <small>Hace puro sueño hasta mañana</small>
+                </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
