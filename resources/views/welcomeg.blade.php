cc90d3198412b0be5cb85e65a45f19ab627a3779
geremyns <geremysaico@gmail.com>
2020-03-25 07:20:46 -0300

    :penguin: Avanze en la pagina

--- resources/views/welcomeg.blade.php
+++ resources/views/welcomeg.blade.php
@@ -8,89 +8,130 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="/css/app.css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-
-        </style>
     </head>
     <body>
-        <div class="row">
-            <div class="col-sm ">
-                <div class="contaniner">
-                    <div class="col t-home img-fluid">
-                        <h2 id="demo" class="sizes shake">Programador</h2>
-                        <h2 class="sizes shake">Bootstrap</h2>
-                        <h2 class="sizes shake">Eloquent</h2>
-                        <h2 class="sizes shake">Laravel</h2>
-                        <h2 class="sizes shake">Vuejs</h2>
-                        <h2 class="sizes shake">CSS</h2>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark disp-mob-none">
+            <!-- Brand -->
+            <a class="navbar-brand" href="#">Programan2Ideas</a>
 
+            <!-- Links -->
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Sobre mi</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contacto</a>
+              </li>
+
+              <!-- Dropdown -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                  Tiendas
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">Tienda de Ventas</a>
+                  <a class="dropdown-item" href="#">Logearse</a>
+                  <a class="dropdown-item" href="#">Link 3</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                  Información
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">Tienda de Ventas</a>
+                  <a class="dropdown-item" href="#">Logearse</a>
+                  <a class="dropdown-item" href="#">Link 3</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                  Deportes
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">Tienda de Ventas</a>
+                  <a class="dropdown-item" href="#">Logearse</a>
+                  <a class="dropdown-item" href="#">Link 3</a>
+                </div>
+              </li>
+            </ul>
+          </nav>
+
+        <div class="contenedor-img">
+            <div class="row ">
+                <div class="col-sm ">
+                    <div class="contaniner">
+                        <div class="col t-home img-fluid">
+                            <h2 id="demo" class="sizes shake">Programador</h2>
+                            <h2 class="sizes shake">Bootstrap</h2>
+                            <h2 class="sizes shake">Eloquent</h2>
+                            <h2 class="sizes shake">Laravel</h2>
+                            <h2 class="sizes shake">Vuejs</h2>
+                            <h2 class="sizes shake">CSS</h2>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm ">
+                    <div class="container ">
+                        <img class="img-fluid float-lefts shake rounded-pill" src="/images/HOME5.png" alt="Mi foto" width="100%" height="100%">
                     </div>
                 </div>
             </div>
-            <div class="col-sm ">
-                <div class="container ">
-                    <img class="img-fluid float-lefts shake" src="/images/HOME.png" alt="Mi foto" width="auto" height="auto">
+        </div>
+        <div class="contenedor-img">
+            <div class="row ">
+                <div class="col-sm ">
+                    <div class="contaniner">
+                        <div class="col t-home img-fluid">
+                            <h2 id="demo" class="sizes shake">Programador</h2>
+                            <h2 class="sizes shake">Bootstrap</h2>
+                            <h2 class="sizes shake">Eloquent</h2>
+                            <h2 class="sizes shake">Laravel</h2>
+                            <h2 class="sizes shake">Vuejs</h2>
+                            <h2 class="sizes shake">CSS</h2>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm ">
+                    <div class="container ">
+                        <img class="img-fluid float-lefts shake rounded-pill" src="/images/HOME5.png" alt="Mi foto" width="100%" height="100%">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="contenedor-img">
+            <div class="row ">
+                <div class="col-sm ">
+                    <div class="contaniner">
+                        <div class="col t-home img-fluid">
+                            <h2 id="demo" class="sizes shake">Programador</h2>
+                            <h2 class="sizes shake">Bootstrap</h2>
+                            <h2 class="sizes shake">Eloquent</h2>
+                            <h2 class="sizes shake">Laravel</h2>
+                            <h2 class="sizes shake">Vuejs</h2>
+                            <h2 class="sizes shake">CSS</h2>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm ">
+                    <div class="container ">
+                        <img class="img-fluid float-lefts shake rounded-pill" src="/images/HOME5.png" alt="Mi foto" width="100%" height="100%">
+                    </div>
                 </div>
             </div>
         </div>
     </body>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
-<script>
- $('#demo').jrumble({
-    rangeX: 10,
-	rangeY: 10,
-	rangeRot: 4
- });
-</script>
+
+
